df383321439b69a4ab70a7dbfd9712159e1c4227
danilom <danilom@gmail.com>
2024-05-08 23:24:21 +0200

    version bump

--- main.c
+++ main.c
@@ -33,7 +33,7 @@
 #define BOOTLOADER_SIZE_KB 48
 
 // BOOT followed by additional info like the version
-#define ENV_STRING "BOOT:v2.1.04"
+#define ENV_STRING "BOOT:v2.1.05"
 
 // The bootloader can be entered in three ways:
 //  - BOOTLOADER_ENTRY_PIN is low
